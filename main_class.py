23eeb904a2fd8ca0f3548711e95886716cbfe9fc
Amin-Bajelan <m.amin.bajelan@gmail.com>
2024-09-13 17:53:06 +0330

    now bot can send message by datetime at a specified time

--- main_class.py
+++ main_class.py
@@ -1,23 +1,50 @@
-# timedelta
+# package
 import telebot
-from telebot import types
-import datetime
-import api_key
 import time
 import requests
+from telebot import types
+from datetime import datetime, timedelta
+
+#classes
 import price_of_cryptocurrency
+import api_key
 
 TOKEN = api_key.API_telegram
 bot = telebot.TeleBot(TOKEN)
 
-continue_state = True
-flag = True
+#flag for live price special cryptocurrency
+
+flag_cryptocurrency = False
+go_to_next_step = False
+final_step = False
+flag_while_cryptocurrency = False
+flag = False  #for while loop
+
+token_name = str
+sleep_time = int
+
+#flag for live price loop
+
+flag_list_price = False
+flag_loop = False
+
+time_for_loop = int
+
+#flag_look_for_another_cryptocurrency
+flag_look_for_another_cryptocurrency = False
 
 
 def stop_loop():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
     btn_stop = types.KeyboardButton('Stop')
     markup.add(btn_stop)
+    return markup
+
+
+def go_back():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    btn_go_back = types.KeyboardButton('Back')
+    markup.add(btn_go_back)
     return markup
 
 
@@ -56,9 +83,10 @@ def create_start_button():
     btn_live_price_loop = types.KeyboardButton('live price loop')
     btn_help = types.KeyboardButton('help')
     btn_info = types.KeyboardButton('info')
-    btn_live_price_special_cryptocurrency = types.InlineKeyboardButton('live price special cryptocurrency')
+    btn_live_price_special_cryptocurrency = types.KeyboardButton('live price special cryptocurrency')
+    btn_look_for_another = types.KeyboardButton('look for another cryptocurrency')
     markup.add(btn_start, btn_live_price, btn_live_price_loop, btn_help, btn_info,
-               btn_live_price_special_cryptocurrency)
+               btn_live_price_special_cryptocurrency, btn_look_for_another)
     return markup
 
 
@@ -70,9 +98,10 @@ def send_welcome(message):
     btn_live_price_loop = types.KeyboardButton('live price loop')
     btn_help = types.KeyboardButton('help')
     btn_info = types.KeyboardButton('info')
-    btn_live_price_special_cryptocurrency = types.InlineKeyboardButton('live price special cryptocurrency')
+    btn_live_price_special_cryptocurrency = types.KeyboardButton('live price special cryptocurrency')
+    btn_look_for_another = types.KeyboardButton('look for another cryptocurrency')
     markup.add(btn_start, btn_live_price, btn_live_price_loop, btn_help, btn_info,
-               btn_live_price_special_cryptocurrency)
+               btn_live_price_special_cryptocurrency, btn_look_for_another)
 
     bot.send_message(message.chat.id, f"Hello 🗿,\nThis robot is designed to see the current prices of "
                                       f"cryptocurrencies.\n\nYou can see "
@@ -82,10 +111,24 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    global check_name
-    global check_time
-    global timer_for_loop
-    global continue_state
+    # flag for live price special cryptocurrency
+    global flag_while_cryptocurrency
+    global flag_cryptocurrency
+    global go_to_next_step
+    global final_step
+    global flag
+
+    global token_name
+    global sleep_time
+
+    # flag for live price loop
+    global flag_list_price
+    global flag_loop
+
+    global time_for_loop
+
+    #for look for another cryptocurrency
+    global flag_look_for_another_cryptocurrency
 
     if message.text == 'start menu':
         bot.send_message(message.chat.id, f"Hello 🗿,\nThis robot is designed to see the current prices of "
@@ -93,8 +136,17 @@ def handle_message(message):
                                           f"list of the top 24 currencies on market by live price button\nOR\nenter "
                                           f"your currency name like ( btc ton xrp )\n\n")
 
-    elif message.text == 'Cancel':
+    elif message.text == 'Cancel' or message.text == 'Stop' or message.text == 'Back':
         bot.send_message(message.chat.id, text="You back to default state: ", reply_markup=create_start_button())
+        flag_while_cryptocurrency = False
+        flag_cryptocurrency = False
+        go_to_next_step = False
+        final_step = False
+        flag = False
+
+        flag_list_price = False
+        flag_loop = False
+
 
     elif message.text == 'live price':
         prices = price_of_cryptocurrency.send_cryptocurrency_price()
@@ -109,28 +161,100 @@ def handle_message(message):
     elif message.text == 'live price loop':
         bot.send_message(message.chat.id, text=f"Please select time of your cryptocurrency loop: ",
                          reply_markup=create_button_time())
+        flag_list_price = True
 
+    if flag_list_price and message.text.isdigit():
+        time_for_loop = int(message.text)
+        flag_loop = True
 
-    elif message.text == 'Stop':
-        create_start_button()
+        current_date = datetime.today()
+        new_date = current_date + timedelta(minutes=time_for_loop)
+        bot.send_message(message.chat.id, text=f"Your loop started: ", reply_markup=stop_loop())
+
+        while flag_loop:
+            time.sleep(10)
+            current_date = datetime.today()
+            if current_date >= new_date:
+                new_date = current_date + timedelta(minutes=time_for_loop)
+                prices = price_of_cryptocurrency.send_cryptocurrency_price()
+                bot.send_message(message.chat.id, f"LIST:\n{prices}")
+            else:
+                print("not yet")
+                print(current_date)
+                print(new_date)
+
 
     elif message.text == 'live price special cryptocurrency':
-        bot.send_message(message.chat.id, text=f"Please enter your cryptocurrency symbol like ( btc ton xrp ):",
+        bot.send_message(message.chat.id, text=f"Please select your cryptocurrency: ",
                          reply_markup=create_button_cryptocurrency_name())
+        flag_cryptocurrency = True
 
+    if message.text == 'BTC' or message.text == 'ETH' or message.text == 'LTC' or message.text == 'XRP' or message.text == 'BCH' or message.text == 'BNB' or message.text == 'EOS' or message.text == 'XLM' or message.text == 'ETC' or message.text == 'TRX':
+        token_name = message.text
 
-    elif message.text.isdigit() == True:
+        bot.send_message(message.chat.id, text=f"Please select your time: ", reply_markup=create_button_time())
+        go_to_next_step = True
 
+    if message.text.isdigit() and flag_cryptocurrency:
+        sleep_time = message.text
 
-    if continue_state == True:
-        name_of_token = str(message.text).upper()
-        my_url = api_key.send_url(message)
+        final_step = True
+    if final_step:
+        bot.send_message(message.chat.id, text=f"your loop started. ", reply_markup=stop_loop())
+        print(token_name)
+        print(sleep_time)
+        name_of_token = token_name.upper()
+        my_url = api_key.send_url(token_name)
         response = requests.get(url=my_url)
+        print(my_url)
+
+        print(my_url)
+
         token_price = float(response.json()['price'])
         token_price = str("{:.3f}".format(round(token_price, 4)))
         bot.send_message(message.chat.id,
-                         f"Current price of {name_of_token} equal:\n\n{name_of_token}  {token_price}\nfor "
-                         f"stop loop enter /break")
+                         f"Current price of {name_of_token} equal:\n\n{name_of_token}   {token_price}\nfor "
+                         f"stop loop press stop button")
+
+        sleep_time_minute = int(sleep_time)
+        sleep_time = int(sleep_time) * 60
+        current_date = datetime.today()
+        new_date = current_date + timedelta(minutes=sleep_time_minute)
+        print(current_date)
+        print(new_date)
+
+        flag = True
+        while flag:
+            time.sleep(10)
+            current_date = datetime.today()
+            if current_date >= new_date:
+                new_date = current_date + timedelta(minutes=sleep_time_minute)
+                token_price = float(response.json()['price'])
+                token_price = str("{:.3f}".format(round(token_price, 4)))
+                bot.send_message(message.chat.id,
+                                 f"Current price of {name_of_token} equal:\n\n{name_of_token}   {token_price}\nfor "
+                                 f"stop loop press button stop")
+            else:
+                print('\nnot yet')
+                print(current_date)
+                print(new_date)
+
+    if message.text == 'look for another cryptocurrency':
+        bot.send_message(message.chat.id, text=f"Please enter symbol of your cryptocurrency like (btc, xrp, ton): ",
+                         reply_markup=go_back())
+        flag_look_for_another_cryptocurrency = True
+
+
+    else:
+        name_of_token = str(message.text).upper()
+        my_url = api_key.send_url(message.text)
+        response = requests.get(url=my_url)
+        if response.status_code == 200 and flag_look_for_another_cryptocurrency:
+            print(message.text)
+            token_price = float(response.json()['price'])
+            token_price = str("{:.3f}".format(round(token_price, 4)))
+            bot.send_message(message.chat.id,
+                             f"Current price of {name_of_token} equal:\n\n{name_of_token}   {token_price}\nfor go to first step press button back: ")
 
 
 bot.polling()
